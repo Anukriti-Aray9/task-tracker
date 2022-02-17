187a3a45093b7518c15aa031125fb91f735f5fde
Anukriti-Aray9 <anu48a@gmail.com>
2022-02-17 11:57:42 +0530

    Initial commit

---
+++ src/main/java/com/comviva/app/service/TaskService.java
@@ -0,0 +1,26 @@
+package com.comviva.app.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.comviva.app.repository.TaskRepository;
+import com.comviva.app.dto.Task;
+@Service
+public class TaskService {
+	
+	@Autowired
+	private TaskRepository taskRepository;
+	
+	public List<Task> listTasks(){
+		return taskRepository.findAll();
+	}
+	
+	public void saveTask(Task task) {
+		//validate data is valid or not
+		//if valid
+		taskRepository.save(task);
+	}
+
+}
